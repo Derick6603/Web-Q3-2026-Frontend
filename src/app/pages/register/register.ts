815ae4b48cc60cb8a16c605bcfaa3e1f9b1120b1
Your Name <djcacontreras@gmail.com>
2026-09-12 01:08:36 -0600

    Register component done

---
+++ src/app/pages/register/register.ts
@@ -0,0 +1,49 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, MatInputModule, MatButtonModule, MatCardModule, RouterLink],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class RegisterComponent {
+  // 3 variables, inputs, fullName, email, password
+  fullName: string = '';
+  email: string = '';
+  password: string = '';
+
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  isLoading: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  onRegister() {
+    this.errorMessage = '';
+    this.successMessage = '';
+    this.isLoading = true;
+
+    this.authService.register(this.fullName, this.email, this.password).subscribe({
+      next: (result) => {
+        this.successMessage = 'Cuenta creada. Ahora inicia sesion.';
+        this.isLoading = false;
+        setTimeout(() => this.router.navigate(['/login']), 1500);
+      },
+      error: (err) => {
+        this.errorMessage = err.error?.message || 'Error al registrar, Intenta de nuevo';
+        this.isLoading = false;
+      },
+    });
+  }
+}
